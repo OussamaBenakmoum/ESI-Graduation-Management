59fdc82b33bbb87380aaa14251468ebbe464aa8c
OussamaBenakmoum <io_benakmoum@esi.dz>
2022-02-19 22:18:40 +0100

    First config, creneau/demande Models creation

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ESI management second",
+
+    'summary': """esi gestion""",
+
+    'description': """
+        desctiption here         
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+
+
+class Creneau(models.Model):
+    _name = 'graduation.creneau'
+    _description = "créneaux des soutenances"
+
+    description = fields.Char(string="description creneau", required=True)
+
+class Demande(models.Model):
+    _name = 'graduation.demande'
+    _description = "demandes de soutenance"
+
+    firststudent = fields.Char(string="Premier étudiant", required=True)
+    secondstudent = fields.Char(string="Deuxieme étudiant", required=False)
+    projecttitle = fields.Char(string="Titre projet", required=True)
+    linkpfe = fields.Char(string="Lien vers le PFE", required=True)
+    supervisor = fields.Char(string="Nom promoteur", required=True)
+    supervisormark = fields.Char(string="Note de sperviseur", required=True)
+    date_prevue = fields.Date(string="Date prévue")
+
