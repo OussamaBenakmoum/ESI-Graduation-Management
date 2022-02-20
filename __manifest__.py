bbd5f42b6d9be8580630c641b84d3f3be7c8b955
OussamaBenakmoum <io_benakmoum@esi.dz>
2022-02-20 01:48:23 +0100

    model security access done, need to fix record access

--- __manifest__.py
+++ __manifest__.py
@@ -22,6 +22,7 @@
 
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/demande.xml'
     ],
--- models.py
+++ models.py
@@ -2,11 +2,6 @@ from odoo import models, fields, api
 from datetime import datetime, timedelta
 
 
-class Creneau(models.Model):
-    _name = 'graduation.creneau'
-    _description = "créneaux des soutenances"
-
-    description = fields.Char(string="description creneau", required=True)
 
 class Demande(models.Model):
     _name = 'graduation.demande'
@@ -20,3 +15,11 @@ class Demande(models.Model):
     supervisormark = fields.Char(string="Note de sperviseur", required=True)
     date_prevue = fields.Date(string="Date prévue")
 
+
+class Creneau(models.Model):
+    _name = 'graduation.creneau'
+    _inherit = 'graduation.demande'
+    _description = "créneaux des soutenances"
+
+    description = fields.Char(string="description creneau", required=True)
+    descriptiondeux = fields.Char(string="description deux creneau", required=True)
